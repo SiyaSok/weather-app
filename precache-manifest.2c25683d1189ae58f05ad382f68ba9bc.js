99e6b5dba900d2d3c77a90c4a5c547186b288713
Ndofire <siyandasokhela@gmail.com>
2020-10-13 20:04:41 +0200

    Updates

---
+++ precache-manifest.2c25683d1189ae58f05ad382f68ba9bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad527b0b08b4f2d8c39e3878f8928539",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "a070ed2312f99d3459e8",
+    "url": "/weather-app/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "acbff95d7da08285fd51",
+    "url": "/weather-app/static/css/main.5f15076f.chunk.css"
+  },
+  {
+    "revision": "a070ed2312f99d3459e8",
+    "url": "/weather-app/static/js/2.7ae2acd8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.7ae2acd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acbff95d7da08285fd51",
+    "url": "/weather-app/static/js/main.552b0bcd.chunk.js"
+  },
+  {
+    "revision": "143bf1c3ab01288e5a6c",
+    "url": "/weather-app/static/js/runtime-main.13c47b1c.js"
+  }
+]);
